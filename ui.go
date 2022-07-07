1a534617428957fc89188419da58391316b380b1
Youngwook-Jeon <ela87@naver.com>
2022-07-07 16:45:14 +0900

    Create a toolbar

--- ui.go
+++ ui.go
@@ -11,8 +11,12 @@ func (app *Config) makeUI() {
 
 	app.PriceContainer = priceContent
 
+	// get toolbar
+	toolBar := app.getToolBar()
+	app.ToolBar = toolBar
+
 	// add container to window
-	finalContent := container.NewVBox(priceContent)
+	finalContent := container.NewVBox(priceContent, toolBar)
 
 	app.MainWindow.SetContent(finalContent)
 }
