ac85c2a3f38a69fcbeefb32ab71c177adeb577e6
Youngwook-Jeon <ela87@naver.com>
2022-07-22 16:00:47 +0900

    Get holdings and display them on the Holdings tab

---
+++ holdings-tab.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"goldobserver/repository"
+	"strconv"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
+)
+
+func (app *Config) holdingsTab() *fyne.Container {
+	return nil
+}
+
+func (app *Config) getHoldingsTable() *widget.Table {
+	return nil
+}
+
+func (app *Config) getHoldingSlice() [][]interface{} {
+	var slice [][]interface{}
+
+	holdings, err := app.currentHoldings()
+	if err != nil {
+		app.ErrorLog.Println(err)
+	}
+
+	slice = append(slice, []interface{}{"ID", "Amount", "Price", "Date", "Delete?"})
+
+	for _, x := range holdings {
+		var currentRow []interface{}
+
+		currentRow = append(currentRow, strconv.FormatInt(x.ID, 10))
+		currentRow = append(currentRow, fmt.Sprintf("%d toz", x.Amount))
+		currentRow = append(currentRow, fmt.Sprintf("$%2f", float32(x.PurchasePrice/100)))
+		currentRow = append(currentRow, x.PurchaseDate.Format("2006-01-02"))
+		currentRow = append(currentRow, widget.NewButton("Delete", func() {}))
+
+		slice = append(slice, currentRow)
+	}
+
+	return slice
+}
+
+func (app *Config) currentHoldings() ([]repository.Holdings, error) {
+	holdings, err := app.DB.AllHoldings()
+	if err != nil {
+		app.ErrorLog.Println(err)
+		return nil, err
+	}
+
+	return holdings, nil
+}
